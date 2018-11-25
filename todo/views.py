3b5acd30503a7105b06531ed72fc94a1ea79ff61
Ray Zhu <jz6868@me.com>
2018-11-25 21:43:56 +0000

    rendering data by for and if logic in todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
 
 # Create your views here.
 def say_hello(request):
     return HttpResponse("Hello World")
 
 def get_todo_list(request):
-    return render(request, "todo_list.html")
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items': results})
+    
+    
