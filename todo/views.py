475f019a81dc3cb86ebccd4b2094a79983700244
Ray Zhu <jz6868@me.com>
2018-11-25 18:09:13 +0000

    add template folder and html file

--- todo/views.py
+++ todo/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def say_hello(request):
     return HttpResponse("Hello World")
+
+def get_todo_list(request):
+    return render(request, "todo_list.html")
