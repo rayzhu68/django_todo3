e4b253f0caac2f5e5ab7bedcce551e789c99230d
Ray Zhu <jz6868@me.com>
2018-11-26 23:27:20 +0000

    Prep settings and requirements for development at Heroku and GitHub

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +76,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+DATABASES = {'default': dj_database_url.parse("postgres://rxfnjxvvpgympk:9db0a9a0441829e549755b3aa5952a9e27cde62dfcb3adc8da6b96e8f9e6c09a@ec2-54-246-85-234.eu-west-1.compute.amazonaws.com:5432/dcdn5l597tv45g")}
 
 
 # Password validation
