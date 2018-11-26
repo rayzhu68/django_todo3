6c896b84c495a49d4f0641015d1c4cd13ddb3563
Ray Zhu <jz6868@me.com>
2018-11-26 00:00:12 +0000

    django forms, use forms.py and modify item_form.html

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello World")
+#def say_hello(request):
+#   return HttpResponse("Hello World")
 
 def get_todo_list(request):
     results = Item.objects.all()
@@ -11,12 +12,13 @@ def get_todo_list(request):
     
 def create_an_item(request):
     if request.method=="POST":
-        new_item = Item()
-        new_item.name = request.POST.get('name')
-        new_item.done = 'done' in request.POST
-        new_item.save()
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
         return redirect(get_todo_list)
+    else:
+        form = ItemForm()
         
-    return render(request, "item_form.html")
+    return render(request, "item_form.html", {'form': form})
     
     
