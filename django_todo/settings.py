f5db65767414f32f5aec3b14d1bad6eb09671d8f
Ray Zhu <jz6868@me.com>
2018-11-28 06:18:17 +0000

    default secret_key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'dh=%75-ict)g0f(+099ri42s&g*b48=oy89x17ed+%&^a!ss74'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'dh=%75-ict)g0f(+099ri42s&g*b48=oy89x17ed+%&^a!ss74'
-                           )
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = development
